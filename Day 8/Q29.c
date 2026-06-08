d4419f121e988ed87cb4fe74c359bff330451df5
AdvitaAgarwal <25114cn329@glbitm.ac.in>
2026-06-08 23:24:14 +0530

    Day 8 uploaded

---
+++ Day 8/Q29.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int rows, i, j;
+
+    printf("Enter number of rows: ");
+    if (scanf("%d", &rows) != 1 || rows < 1)
+        return 0;
+
+    for (i = 1; i <= rows; ++i) {
+        for (j = 1; j <= i; ++j) {
+            putchar('*');
+        }
+        putchar('\n');
+    }
+
+    return 0;
+}
